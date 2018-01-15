ef1d04e40a8e2c8c2e61c6a15ddb72f85dea9d6d
ssheaks <sarah.sheaks@gmail.com>
2018-01-15 14:09:08 -0800

    finished exercises

--- practice.js
+++ practice.js
@@ -1,8 +1,33 @@
 'use strict';
-function whoAmI(name, age) {
-  let yearOfBirth = 2018 - age;
-  console.log(`My name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}`);
+function yearOfBirth(age) {
+  try {
+    if (age < 0) {
+      throw new Error('age must be a positive number');
+    }
+    return 2018 - age;
+  } 
+  catch (e) {
+    console.log(e);
+  }
+  
 }
 
-whoAmI('Zak', 33);
+function whoAmI(name, age) {
+  let yob = yearOfBirth(age);
+  if (yob === undefined) {
+    return 'please enter your true age';
+  }
+  if (typeof name !== 'string') {
+    console.error('arguments not valid');
+  }
+  else if (typeof age !== 'number') {
+    console.error('arguments not valid');
+  }
+  else {
+    return `My name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
+  }
+  //console.log(`I was born in ${yob}`);
+}
+
+console.log(whoAmI('Zak', 33));
+yearOfBirth(33);
