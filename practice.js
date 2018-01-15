b053b128ec9d8c787ef7cf95f5719f938284b149
zakdrummond <zakdrummond@gmail.com>
2018-01-15 12:50:35 -0800

    exercise 4

--- practice.js
+++ practice.js
@@ -1,6 +1,8 @@
 'use strict';
 function whoAmI(name, age) {
+  let yearOfBirth = 2018 - age;
   console.log(`My name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI('Zak', 33);
